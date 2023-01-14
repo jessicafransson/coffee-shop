094f2154876ada8327a5dbc0ed4ce78d89b07498
Jessica Fransson <jessicafransson@outlook.com>
2023-01-14 16:46:03 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MLnvRBA9e1q2huTw8ppPJOhZIwp9zDVHyXEzqPN0WTDPmgUoJYvDMD2wdXnTsp3yphDIyUbafvO1e6VoQKbhqkM00mzolrcLh',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
