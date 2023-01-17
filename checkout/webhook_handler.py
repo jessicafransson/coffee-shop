45cbfc5da31e3a1ff14cfb61a65a7e903aae5a1e
Jessica Fransson <jessicafransson@outlook.com>
2023-01-17 10:44:34 +0000

    updates to send email conf

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from products.models import Product
@@ -11,53 +14,72 @@ import time
 
 class StripeWH_Handler:
     """
-    Handling the stripe webooks
+    Handles the Stripe Web Hook
     """
-
     def __init__(self, request):
         self.request = request
 
+    def _send_confirmation_email(self, order):
+        """
+        Send user confirmation email
+        """
+        cust_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order}
+        )
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order,
+                'contact_email': settings.DEFAULT_FROM_EMAIL}
+        )
+        send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
+
     def handle_event(self, event):
         """
-        Here to handle generic, unknown or unexpected webhook events
+        Handles a generic/unknown/unexpected Web Hook event
         """
         return HttpResponse(
-            content=f'Unhandled webhook recieved: {event["type"]}',
-            status=200)
+            content=f'Unhandled Webhook received: {event["type"]}',
+            status=200
+        )
 
     def handle_payment_intent_succeeded(self, event):
         """
-        Here to handle payment intent success webhook
+        Handles the payment intent succeeded WebHook from Stripe
         """
         intent = event.data.object
         pid = intent.id
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        # Get the Charge object
+        # gets the charge object
         stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
-        billing_details = stripe_charge.billing_details  # updated
+        billing_details = stripe_charge.billing_details
         shipping_details = intent.shipping
-        grand_total = round(stripe_charge.amount / 100, 2)  # updated
+        grand_total = round(stripe_charge.amount / 100, 2)
 
-        # keeping clean data in shipping details
+        # clean data from shipping details to prevent empty strings in db
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
 
-        # update profile if save info is checked
+        # Update profile if save_info checked
         profile = None
         username = intent.metadata.username
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
+                profile.default_full_name = shipping_details.name
+                profile.default_street_address1 = (
+                    shipping_details.address.line1)
+                profile.default_street_address2 = (
+                    shipping_details.address.line2)
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_county = shipping_details.address.state
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_country = shipping_details.address.country
                 profile.default_phone_number = shipping_details.phone
-                profile.default_country = shipping_details.country
-                profile.default_postcode = shipping_details.postal_code
-                profile.default_town_or_city = shipping_details.city
-                profile.default_street_address1 = shipping_details.line1
-                profile.default_street_address2 = shipping_details.line2
-                profile.default_county = shipping_details.state
                 profile.save()
 
         order_exists = False
@@ -66,14 +88,14 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
-                    email__iexact=shipping_details.email,
+                    email__iexact=billing_details.email,
                     phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.country,
-                    postcode__iexact=shipping_details.postal_code,
-                    town_or_city__iexact=shipping_details.city,
-                    street_address1__iexact=shipping_details.line1,
-                    street_address2__iexact=shipping_details.line2,
-                    county__iexact=shipping_details.state,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
                     grand_total=grand_total,
                     original_bag=bag,
                     stripe_pid=pid,
@@ -132,8 +154,9 @@ class StripeWH_Handler:
 
     def handle_payment_intent_failed(self, event):
         """
-        Here to handle payment intent failed webhook
+        Handles the payment intent failed WebHook from Stripe
         """
         return HttpResponse(
-            content=f'Webhook recieved: {event["type"]}',
-            status=200)
+            content=f'WebHook received: {event["type"]}',
+            status=200
+        )
