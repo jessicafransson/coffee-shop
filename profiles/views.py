e7443ff5a349bf103575bf84733139a2d6cfd50e
Jessica Fransson <jessicafransson@outlook.com>
2023-01-17 09:07:53 +0000

    updates to update profile information and view order history

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
 
+
+@login_required
 def profile(request):
     """
     View the users profile account
@@ -16,15 +20,35 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your profile updated successfully!')
-
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
+        else:
+            messages.error(request, 'Your update failed, please ensure \
+                your information is correct and try again!')
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all().order_by('-date')
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
         'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a previoud confirmation for order number {order_number}. '
+        'You got a confirmation email sent on the date of the order.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True
     }
 
     return render(request, template, context)
