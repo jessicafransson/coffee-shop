f0f2ce0afa793597a24e59679a4563596ee6d377
Jessica Fransson <jessicafransson@outlook.com>
2023-01-19 09:23:03 +0000

    updates to admin edit and delete items

--- products/views.py
+++ products/views.py
@@ -63,9 +63,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Product added successfully!!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request, 'Could not add your product,\
@@ -92,8 +92,8 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. \
-                Please ensure the form is valid.')
+            messages.error(request, 'Could not update the product. \
+                Please make sure the form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
@@ -105,3 +105,13 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """
+    Delete an item from the shop
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product is now deleted!')
+    return redirect(reverse('products'))
