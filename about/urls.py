eda26e9bf0107c82a0f2c5fb23f6e9bdc2f4f8a8
Jessica Fransson <jessicafransson@outlook.com>
2023-01-27 19:09:26 +0000

    correct setup for the about us pages

--- about/urls.py
+++ about/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.conf.urls import url, include
+
 from . import views
 
 urlpatterns = [
     path('', views.view_about_page, name='about'),
+    path('farm/', views.view_farm_page, name='farm'),
+    path('flavours/', views.view_flavours_page, name='flavours'),
 ]
--- about/views.py
+++ about/views.py
@@ -5,3 +5,15 @@ def view_about_page(request):
     """ A view to return the about us page """
 
     return render(request, 'about/about.html')
+
+
+def view_farm_page(request):
+    """A view to the about us and our farm page"""
+
+    return render(request, 'about/farm.html')
+
+
+def view_flavours_page(request):
+    """A View to the about us and flavours page"""
+
+    return render(request, 'about/flavours.html')
