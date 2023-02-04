2f398cdde67d528ec36c3b994545d09cab4c45a3
Jessica Fransson <jessicafransson@outlook.com>
2023-02-04 13:38:24 +0000

    updates to settings file

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = (
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['coffee_shop.herokuapp.com', 'localhost']
 
 
 # Apps installed and defined
