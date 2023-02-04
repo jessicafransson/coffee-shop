f9d0ed906dc207f491c6ec208933efd2f769fb23
Jessica Fransson <jessicafransson@outlook.com>
2023-02-04 15:32:24 +0000

    removed secret key and set debug

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -22,12 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-$1==(i9e=^2ud6pe@fk+fwtl=@1jiok9_@i&^1wnn77_276pmo'
-)
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['coffeeshop91.herokuapp.com', 'localhost']
 
