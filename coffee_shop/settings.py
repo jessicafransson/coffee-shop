f2048a2c4c31deb6595d89f527140dff18b89228
Jessica Fransson <jessicafransson@outlook.com>
2023-02-06 18:13:22 +0000

    updates to cloudinary

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
