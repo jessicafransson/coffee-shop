0ba79bd93c5aaa044dc60922f257929567bdce71
Jessica Fransson <jessicafransson@outlook.com>
2023-02-12 17:03:29 +0000

    updates to email settings for sending real emails

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -237,6 +237,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
