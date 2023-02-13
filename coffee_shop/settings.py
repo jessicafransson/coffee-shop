a7636a1439f63541488c6e1b67acab9dff54a90d
Jessica Fransson <jessicafransson@outlook.com>
2023-02-13 08:55:36 +0000

    updates to mobile view

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -219,6 +219,9 @@ FREE_DELIVERY_THRESHOLD = 30
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 # stripe variables
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
@@ -241,5 +244,3 @@ else:
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
